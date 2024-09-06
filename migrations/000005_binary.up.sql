fb17d0a3eab3b96efa5ef4aa37f1522e0428acb6
lks-go <alekssum@gmail.com>
2024-09-06 23:43:51 +0300

    binary data migrations

--- migrations/000005_binary.up.sql
+++ migrations/000005_binary.up.sql
@@ -1,13 +1,9 @@
-CREATE TABLE card (
+CREATE TABLE binary_data (
     id                      SERIAL NOT NULL PRIMARY KEY,
     owner                   UUID NOT NULL,
     title                   VARCHAR(255) NOT NULL,
-    encrypted_number        TEXT NOT NULL,
-    encrypted_owner         TEXT NOT NULL,
-    encrypted_exp_date      TEXT NOT NULL,
-    encrypted_cvc_code      TEXT NOT NULL,
     created_at              TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at              TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
-CREATE INDEX ON card (owner);
+CREATE INDEX ON binary_data (owner);
